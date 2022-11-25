39819ead08dba8d66bc4dbae519ebeffb0cec162
chri923t <chri923t@stud.kea.dk>
2022-11-25 17:36:07 +0100

    change

--- main.js
+++ main.js
@@ -2,30 +2,79 @@ fetch('http://localhost:3000/cafes')
     .then (response => response.json())
     .then(function(cafes){
         rendercafeList(cafes);
+        registerEvents(cafes);
     });
+
 function rendercafeList(cafes) {
     console.log(cafes)
-    const ul = document.querySelector('ul#cafeList');
+
+    const ul = document.querySelector('ul#cafes');
     console.log(ul);
+
     ul.innerHTML = '';
     for (let i = 0; i < cafes.length; i++) {
         const cafe = cafes[i];
-        console.log(cafe)
+        console.log(cafes)
+
+        const li = document.createElement('li');
+        li.innerHTML = `
+    <p class="cafeName">Cafe Name: ${cafe.cafe_name}</p>
+        <p class="cafeOpen">Opening Hour: ${cafe.opening_hour}</p> 
+        <p class="cafelocation">Location: ${cafe.location}</p>
+        <p class="cafeNumber">Telephone Number: ${cafe.cafe_number}</p>`;
+        ul.appendChild(li);
     }
-    const li = document.createElement('li');
-    li.innerHTML = `
-    <p class="text">${cafes.text}</p>`;
-    ul.appendChild(li);
+
 }
 
-function registerEventlisteners (cafes) {
-    const button = document.querySelector('button.minimum');
+
+function registerEvents (cafes) {
+    const button = document.querySelector('button.filter');
     console.log(button)
-    const minimumInput = document.querySelector('input');
     button.addEventListener ('click', function (){
-        const minimumPrice = minimumInput.value;
-        const filtercafes = cafes.filter(cafe => parseInt(cafe.price) > minimumPrice);
-        rendercafeList(filtercafes);
+        const wifiInput = document.querySelector('input#wifi');
+        const musicInput = document.querySelector('input#music');
+        const priceInput = document.querySelector('input#prices');
+        const cafenameInput = document.querySelector('input#cafename');
+        const noiseInput = document.querySelector('input#noise');
+        const chairInput = document.querySelector('input#chairs');
+        const couchInput = document.querySelector('input#couch');
+        console.log(wifiInput, musicInput, priceInput, cafenameInput, noiseInput, chairInput, couchInput);
+        const minimumWifi = wifiInput.value;
+        const minimumMusic = musicInput.value;
+        const minimumPrices = priceInput.value;
+        const minimumCafename = cafenameInput.value;
+        const minimumnoise = noiseInput.value;
+        const minimumChair = chairInput.value;
+        const minimumCouch = couchInput.value;
+
+        const filteredCafes = [];
+        for (let i = 0; i < cafes.length; i++) {
+            debugger;
+            const cafe = cafes[i];
+            if((cafe.prices) === (minimumPrices)){
+                filteredCafes.push(cafe);}
+            if((cafe.wifi) === (minimumWifi)){
+                filteredCafes.push(cafe);
+            }
+
+
+        }
+        rendercafeList(filteredCafes);
     });
 
 }
+fetch('http://localhost:3000/cafes', {
+    method: "POST",
+    body: JSON.stringify(cafe),
+    headers: {
+        "Content-Type": "application/json"
+    }
+})
+    .then((response) => response.json())
+    .then(function (isSuccessfulString) {
+        console.log(isSuccessfulString);
+    });
+
+
+
